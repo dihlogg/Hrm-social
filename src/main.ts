299b49624bec44403b2309b7324e79b40b39e3a5
dihlogg <dinhlongg171204@gmail.com>
2026-06-22 23:48:09 +0700

    update cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: true,
+    origin: ['http://localhost:3000', 'https://hrm-tool.vercel.app', 'https://ltdhrm.me', 'https://www.ltdhrm.me'],
     credentials: true,
   });
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
